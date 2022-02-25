e7289f5014eb7d232ec4fb7932246ba306b6c816
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-02-25 09:36:23 -0300

    Uploads Studies

---
+++ SimpleCondition1.java
@@ -0,0 +1,19 @@
+
+public class SimpleCondition1 {
+	public static void main(String [] args) {
+		double number1 = 0;
+		
+		double doub = (number1 * 2);
+		double triple = (number1 * 3);
+		
+		if(number1 > 0) {
+			System.out.println("O seu resultado é: " + doub);
+		}
+		else {
+			System.out.println("O seu resultado é: " + triple);
+		}
+		
+	}
+
+
+}
