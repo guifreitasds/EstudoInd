beea816900a5a57a301fb19ff7569ef3a47fc627
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-02-25 13:12:21 -0300

    Add files via upload

---
+++ AgeScanner.java
@@ -0,0 +1,25 @@
+
+public class AgeScanner {
+	public static void main (String [] args) {
+		double agey = 17;
+		double agem = 3;
+		double aged = 19;
+		double yd;
+		double md;
+		double dd;
+		
+		yd = (agey * 365);
+		md = (agem*30);
+		dd = (aged);
+		
+		
+		double Scanner;
+
+		Scanner = (yd + md + dd);
+		
+		System.out.println(Scanner + "dias");
+		
+	
+	}
+
+}
---
+++ MediaArit1.java
@@ -0,0 +1,36 @@
+
+public class MediaArit1 {
+
+	public static void main(String[] args) {
+		int n1 = 8;
+		int n2 = 9;
+		int n3 = 7;
+		
+		int n4 = 4;
+		int n5 = 5;
+		int n6 = 6;
+
+
+		double media1;
+		double media2;
+		double media3;
+		double media4;
+		
+		media1 = ((n1 + n2 + n3)/3);
+		media2 = ((n4 + n5 + n6)/3);
+		media3 = (media1 + media2);
+		media4 = ((media1 + media2)/2);
+		
+		System.out.println("A sua média inicial é: " + media1);
+		System.out.println("A sua segunda média é: " + media2);
+		System.out.println("A soma das médias é: " + media3);
+		System.out.println("A média das médias é: " + media4);
+		
+		
+				
+	}
+
+}
+
+
+
