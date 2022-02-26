cb44363eb3c1bae06db13ea5876f3cdb4474ae13
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-02-26 10:46:24 -0300

    + Prática java

---
+++ minimumsalarycalculator.java
@@ -0,0 +1,14 @@
+
+public class minimumsalarycalculator {
+	public static void main(String [] args) {
+		double msalary = 1.200;
+		double personsalary = 38.000;
+		
+		double manysalaries;
+		
+		manysalaries = (personsalary/msalary);
+		
+		System.out.println("O empregado recebe: " + manysalaries + " salários mínimos");
+	}
+
+}
