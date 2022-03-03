9bd5b6e90d407a1efaa28d3a9dcd4858bb442967
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-03-03 10:03:34 -0300

    TConversion with Scanner utilitary

--- TConversion.java
+++ TConversion.java
@@ -1,9 +1,13 @@
-
+import java.util.Scanner;
 public class TConversion {
 	public static void main(String[] args) {
+		
 		double C, F, K, Re, Ra;
 		
-		C = 30;
+		Scanner Celsius = new Scanner(System.in);
+		System.out.println("Digite a temperatura: ");
+		C = Celsius.nextDouble();
+		
 		
 		F = (C * 1.8 + 32);
 		K = (C + 273.15);
@@ -14,6 +18,8 @@ public class TConversion {
 		System.out.println("O valor da temperatura em Kelvin é: " + K);
 		System.out.println("O valor da temperatura em Réaumur é: " + Re);
 		System.out.println("O valor da temperatura em Rankine é: " + Ra);
+		
+		Celsius.close();
 	}
 
 }
