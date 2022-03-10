520d821f77ce3e9dba1458738e628312cdb2c2a9
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-03-10 13:26:32 -0300

    Multiplication w/ Python

---
+++ Multiplication.py
@@ -0,0 +1,13 @@
+# Estudo IND
+# Multiplication Calculator
+
+print('Vamos multiplicar?')
+
+value1 = float(input('Escreva o primeiro valor aqui: '))
+value2 = float(input('Escreva o segundo valor aqui: '))
+
+multi = value1 * value2
+
+p = ('O resultado da sua multiplicação é: ' + str(multi))
+
+print(p)
