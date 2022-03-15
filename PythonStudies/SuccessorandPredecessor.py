38f5108bf5191d2e3951a1002be086f5538bd5f7
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-03-15 10:12:36 -0300

    Adding Python Studies

---
+++ PythonStudies/SuccessorandPredecessor.py
@@ -0,0 +1,12 @@
+#Successor and predecessor numbers
+
+number = int(input('Insira o número desejado: '))
+
+successor = (number + 1)
+predecessor = (number - 1)
+
+s = ('O sucessor desse número é: ' + str(successor))
+p = ('O antecessor desse número é: ' + str(predecessor))
+
+print(s)
+print(p)
