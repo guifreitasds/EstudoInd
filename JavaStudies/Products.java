40e856f3dfa4f8386f07dc262b8fb4b5a96aa86e
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-03-17 10:34:30 -0300

    Product Verifier w/ Java

---
+++ JavaStudies/Products.java
@@ -0,0 +1,56 @@
+package com.dawclass;
+import java.util.Scanner;
+
+public class Products {
+	public static void main (String [] args) {
+		double local, value, finalvalue;
+		double local1 = 1;
+		double local2 = 2;
+		double cotation = 5.09;
+		String name;
+		Scanner util = new Scanner(System.in);
+		
+		
+		System.out.println("Checagem de Produtos");
+		System.out.println("Insira o nome do seu produto: ");
+		name = util.nextLine();
+		
+		while(name == "Anyone" ) {
+			System.out.println("Insira um nome: ");
+			name = util.nextLine();
+		}
+		
+		System.out.println("Insira o valor do produto: ");
+		value = util.nextDouble();
+		
+		while(value < 0) {
+		    System.out.println("Insira um valor válido:  ");
+		    value = util.nextDouble();
+		}
+		
+		System.out.println("Insira 1 para produto importado e 2 para produto nacional");
+		local = util.nextDouble();
+		
+		while(local != 1 && local != 2) {
+			System.out.println("Insira alguma numeração válida:  ");
+			local = util.nextDouble();
+		}
+		
+		if(local == local1) {
+			finalvalue = (value * cotation);
+			System.out.println("O valor final de " + name + " é: R$" + finalvalue);
+		}
+		else if(local == local2) {
+			System.out.println("O valor final de " + name + " é: R$" + value);
+			
+		}
+		else {
+			System.out.println("Insira um valor de local válido!");
+			
+		}
+		
+		util.close();
+	}
+	
+
+}
