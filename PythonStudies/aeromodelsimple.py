279c1016297ed83c4c9666f02ce3c1d5682588c9
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-03-22 13:50:42 -0300

    Aero Model new version w/ Python

--- PythonStudies/aeromodelsimple.py
+++ PythonStudies/aeromodelsimple.py
@@ -1,34 +1,53 @@
 #Aero Model
 
-
-plane = float(input("Vamos pilotar um avião? Digite 1 para acelerar o avião: "))
-
-
-if plane == 1:
+# Criação do primeiro loop, para o primeiro comando
+while True:
+  # Entrada do comando (numérico)
+ plane = float(input("Vamos pilotar um avião? Digite 1 para acelerar o avião: "))
+  # Uso de estrutura condicional para validar o comando
+ if plane == 1:
   print('O avião está acelerando')
   continue1 = float(input("Digite 2 para decolar o avião: "))
-else:
+  # Quebra do loop para saida, e também entrada no próximo loop
+  break
+ else:
   print('O avião está parado')
+# Fim do primeiro loop
 
-if continue1 == 2:
+# Começo do segundo loop
+while True: 
+ if continue1 == 2:
   print('O avião decolou')
   continue2 = float(input('Digite 3 para ligar o piloto automático: '))
-else:
+  break
+ else:
   print('O avião ainda está decolando!')
+# Fim do segundo loop
 
-if continue2 == 3:
+# Começo do terceiro loop
+while True:
+ if continue2 == 3:
   print('Você ligou o piloto automático!')
   continue3 = float(input('Digite 4 para desacelerar e pousar: '))
-else:
+  break
+ else:
   print('O avião ainda está decolando')
+# Fim do terceiro loop
 
-if continue3 == 4:
+# Começo do quarto loop
+while True: 
+ if continue3 == 4:
   print('O avião foi pousado!')
   continue4 = float(input('Digite 5 para desligar o avião: '))
-else: 
+  break
+ else: 
   print('O avião continua funcionando')
+# Fim do quarto loop
 
-if continue4 == 5:
+# Começo do quinto loop
+while True:
+ if continue4 == 5:
   print('Parabéns, você pousou e desligou o avião com sucesso!!!')
-
+  break
+# fim do codigo
   
