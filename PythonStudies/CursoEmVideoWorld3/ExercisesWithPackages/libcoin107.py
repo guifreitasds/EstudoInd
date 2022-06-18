51be363d29c2f275d75d39941000f22e59ea483f
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2022-06-18 13:43:58 -0300

    Add files via upload

---
+++ PythonStudies/CursoEmVideoWorld3/ExercisesWithPackages/libcoin107.py
@@ -0,0 +1,15 @@
+def metade(num):
+    res = num / 2
+    return res
+
+def dobro(num):
+    res = num * 2
+    return res
+
+def aumentar(num, percent):
+    res = (num+(num*(percent/100)))
+    return res
+
+def diminuir(num, percent):
+    res = (num-(num*(percent/100)))
+    return res
