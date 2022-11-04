54c09b3e67a0fafd8028ba81b19244e592cf7f06
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-11-04 16:19:32 -0300

    Ex033

--- PythonStudies/ExtraExercises/ex033.py
+++ PythonStudies/ExtraExercises/ex033.py
@@ -1,3 +1,8 @@
 from basiclibs.extras import *
 
-verificarMedia(7,2,4)
+n1 = leiaInt('Digite o número a ser analisado: ')
+
+if(n1%2==0):
+    print(f'Número {n1} par')
+else:
+    print(f'Número {n1} ímpar')
