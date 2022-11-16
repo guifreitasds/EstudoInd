e41aa43bdf32f77324a439dd324a58191e282425
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-11-16 16:08:08 -0300

    teste008

---
+++ MobileStudies/DartStudies/teste008.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+void main() {
+  BuscarFuncionarios buscarFunc = BuscarFuncionarios();
+  buscarFunc();
+}
+
+class BuscarFuncionarios{
+
+  void call() => print("Alberto, Adalberto e Cláudio");
+}
