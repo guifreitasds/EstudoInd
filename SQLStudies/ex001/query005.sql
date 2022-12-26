ed3b122ee64fff24a68f2b36ad9e5db78132a29d
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-26 13:06:37 -0300

    ALTER TABLE command 1, 2, 3

---
+++ SQLStudies/ex001/query005.sql
@@ -0,0 +1,13 @@
+alter table pessoas
+add column profissao varchar(10);
+
+select * from pessoas;
+
+alter table pessoas
+drop column profissao;
+
+alter table pessoas
+modify column profissao varchar(20);
+
+alter table pessoas
+change column prof profissao varchar(20);
