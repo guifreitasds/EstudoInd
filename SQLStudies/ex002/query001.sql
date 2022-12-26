02c4e6cc58cbe42f3306fca4995fd443458996f9
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-26 13:15:47 -0300

    CREATE TABLE IF NOT EXISTS command

---
+++ SQLStudies/ex002/query001.sql
@@ -0,0 +1,7 @@
+create table if not exists cursos(
+	nome varchar(30) not null unique,
+    descricao text,
+    carga int unsigned,
+    totaulas int unsigned,
+    ano year default '2023'
+) default charset=utf8; 
