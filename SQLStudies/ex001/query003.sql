85e2d08c061e5b0b1cd41135986f01e2d15a52e6
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-26 10:54:51 -0300

    INSERT INTO command with many registers

--- SQLStudies/ex001/query003.sql
+++ SQLStudies/ex001/query003.sql
@@ -1,4 +1,4 @@
 INSERT INTO pessoas values
-('Alejandro', '1980-11-25', 'M', '75.5', '1.78', 'Argentina');
+(default, 'Alejandro', '1980-11-25', 'M', '75.5', '1.78', 'Argentina');
 
 select * from pessoas;
---
+++ SQLStudies/ex001/query004.sql
@@ -0,0 +1,9 @@
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('João', '1987-06-20', 'M', '81.4', '1.76', default),
+('Marcos', '1999-06-22', 'M', '100.5', '2.01', default),
+('Ana', '2000-12-20','F', '60.2','1.65', default),
+('Mila', '2005-02-10', 'F', '49.6', '1.57', default);
+
+select * from pessoas;
