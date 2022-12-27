1d0e63b82ae41766a6296000fcb5ae0dc8cdda1d
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-27 13:33:08 -0300

    Manipulando linhas com UPDATE SET WHERE

--- SQLStudies/ex002/query002.sql
+++ SQLStudies/ex002/query002.sql
@@ -13,3 +13,9 @@ values
 ('Youtuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
 
 select * from cursos;
+
+update cursos set nome='HTML5' where idcurso ='1';
+
+update cursos set nome='PHP', ano='2015' where idcurso='4';
+
+update cursos set nome='Java', ano='2015', carga='40' where idcurso='5'; 
