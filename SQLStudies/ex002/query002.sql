3568f57707e606620c0a48fd8e4f8e01efe59e54
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-27 13:26:08 -0300

    inserindo valores a ser manipulados

---
+++ SQLStudies/ex002/query002.sql
@@ -0,0 +1,15 @@
+insert into cursos 
+(nome, descricao, carga, totaulas, ano)
+values
+('HTML4', 'CURSO DE HTML5', '40', '37', '2014'),
+('Algoritmos', 'Lógica de programação', '20', '15', '2014'),
+('Photoshop', 'Dicas de photoshop CC', '10', '8', '2014'),
+('PGP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+('Jarva', 'Introdução à linguagem JAVA', '10', '29', '2000'),
+('MySQL', 'Banco de dados MySQL', '30', '15', '2016'),
+('Word', 'Curso completo de word', '40', '30', '2016'),
+('Sapateado', 'Danças ritmicas', '40', '30', '2018'),
+('Cozinha árabe', 'Aprenda a fazer kibe', '40', '30', '2018'),
+('Youtuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
+
+select * from cursos;
