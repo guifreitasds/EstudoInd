55e9f4e6e149392b45a9ef3c572c743a90927678
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-04 10:43:09 -0300

    ex005

--- SQLStudies/ex004/query004.sql
+++ SQLStudies/ex004/query004.sql
@@ -3,3 +3,8 @@ select * from gafanhotos where sexo = 'F';
 select * from gafanhotos where nascimento between '2000-01-01' and '2015-12-31';
 
 select * from gafanhotos where profissao = 'Programador' and sexo = 'M';
+
+select * from gafanhotos where nacionalidade = 'Brasil' and nome like 'J%' and sexo = 'F';
+
+select nome, nacionalidade from gafanhotos where sexo = 'M' and nome like '%Silva%' and nacionalidade <> 'Brasil' and peso < 100;
+
