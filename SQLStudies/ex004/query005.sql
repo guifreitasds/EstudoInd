a3d7929dc0e87d4815fd2608817a379146d8d3e1
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-04 13:20:59 -0300

    comando SELECT GROUP BY with HAVING condition and adding SELECT

--- SQLStudies/ex004/query005.sql
+++ SQLStudies/ex004/query005.sql
@@ -4,3 +4,10 @@ select totaulas, count(*) from cursos group by totaulas;
 
 select ano, count(*) as qtd_cursos from cursos 
 group by ano having qtd_cursos>=4 order by count(*) desc;
+
+select avg(carga) from cursos;
+
+select carga, count(*) as qtd_cursos from cursos
+where ano > 2015
+group by carga
+having carga > (select avg(carga) from cursos);
