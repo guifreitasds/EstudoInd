c36bdfb70e271944ba4d96c12f23f02ea8177481
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-04 13:13:41 -0300

    comando SELECT GROUP BY with HAVING condition

--- SQLStudies/ex004/query005.sql
+++ SQLStudies/ex004/query005.sql
@@ -1,3 +1,6 @@
 select carga, count(nome) as qtd_cursos from cursos group by carga;
 
 select totaulas, count(*) from cursos group by totaulas;
+
+select ano, count(*) as qtd_cursos from cursos 
+group by ano having qtd_cursos>=4 order by count(*) desc;
