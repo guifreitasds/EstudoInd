8eef4e71663f92e6322379da8ee8776c9d6c5dbd
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-04 13:33:45 -0300

    ex004

--- SQLStudies/ex004/query006.sql
+++ SQLStudies/ex004/query006.sql
@@ -10,3 +10,10 @@ select nacionalidade, count(*) as qtd_pessoas from gafanhotos
 where nacionalidade != 'Brasil'
 group by nacionalidade 
 having qtd_pessoas > 3;
+
+select avg(altura) from gafanhotos;
+
+select altura, count(*) as qtd_pessoas from gafanhotos
+where peso>100
+group by altura 
+having altura > (select avg(altura) from gafanhotos);
