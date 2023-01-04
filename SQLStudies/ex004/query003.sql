a24a338ae2d7680b54f6060d8120ccd0d5366ec6
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-04 10:29:14 -0300

    comando SELECT MAX and MIN parameters

--- SQLStudies/ex004/query003.sql
+++ SQLStudies/ex004/query003.sql
@@ -2,3 +2,13 @@ select * from gafanhotos
 where nome like '%Silva%';
 
 select distinct nacionalidade from gafanhotos;
+
+
+select count(*) from cursos;
+
+select count(*) from cursos 
+where carga > 40;
+
+select max(totaulas) from cursos;
+
+select min(carga) from cursos;
