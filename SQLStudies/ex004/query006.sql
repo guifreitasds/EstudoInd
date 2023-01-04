3eb422c7c55a1a573cc608f7ba79c798231d99c2
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-04 13:29:02 -0300

    ex003

--- SQLStudies/ex004/query006.sql
+++ SQLStudies/ex004/query006.sql
@@ -5,3 +5,8 @@ order by qtd_pessoas desc;
 select sexo, count(*) as qtd_pessoas from gafanhotos
 where nascimento > '2005-01-01'
 group by sexo;
+
+select nacionalidade, count(*) as qtd_pessoas from gafanhotos
+where nacionalidade != 'Brasil'
+group by nacionalidade 
+having qtd_pessoas > 3;
