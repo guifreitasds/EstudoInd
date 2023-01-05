fa1ecc3626c5f7eb4df84afae1c87f10d3e93ee6
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-05 16:33:19 -0300

    RIGHT and LEFT JOIN usability

--- SQLStudies/ex005/query002.sql
+++ SQLStudies/ex005/query002.sql
@@ -2,3 +2,13 @@ select g.nome, c.nome as nomecurso from gafanhotos as g
 join cursos as c
 on c.idcurso = g.cursopreferido
 order by nomecurso;
+
+select g.nome, c.nome as nomecurso from gafanhotos as g
+left join cursos as c
+on c.idcurso = g.cursopreferido
+order by nomecurso;
+
+select g.nome, c.nome as nomecurso from gafanhotos as g
+right join cursos as c
+on c.idcurso = g.cursopreferido
+order by nomecurso;
