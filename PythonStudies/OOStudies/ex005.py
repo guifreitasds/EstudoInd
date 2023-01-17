33149843136502d5f8b4e77da2c136c2acb467d0
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-17 10:40:14 -0300

    creating sell method

--- PythonStudies/OOStudies/ex005.py
+++ PythonStudies/OOStudies/ex005.py
@@ -22,11 +22,19 @@ in stock? {self.in_stock()}""")
         else:
             return False
 
+    def sell(self, qtd=1):
+        if self.in_stock():
+            self.copies-=qtd
+        else:
+            print(f'Book out of order')
 
 
-book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,0)
+
+book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,5)
 book2 = Book('652-6-86-748413-3', 'Learn Chemistry','Jack', 'CBC', 400, 220,20)
 book3 = Book('957-7-39-347216-2', 'Learn Maths','John', 'XYZ', 500, 300,5)
 book4 = Book('957-7-39-347216-2', 'Learn Biology','Jack', 'XYZ', 400, 200,6)
 
+
+book1.sell(2)
 book1.display()
