8243d0e8c04ce1155b0f339f33da07942f462f54
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-17 13:01:19 -0300

    creating price property and setter

--- PythonStudies/OOStudies/ex005.py
+++ PythonStudies/OOStudies/ex005.py
@@ -30,21 +30,22 @@ in stock? {self.in_stock()}
         else:
             print(f'Book out of order')
 
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, val):
+        if 50<=val<=1000:
+            self._price=val
+        else:
+            raise ValueError('Price cannot be more than 1000 and less than 50')
 
 
-book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,5)
+book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,10)
 book2 = Book('652-6-86-748413-3', 'Learn Chemistry','Jack', 'CBC', 400, 220,20)
 book3 = Book('957-7-39-347216-2', 'Learn Maths','John', 'XYZ', 500, 300,5)
 book4 = Book('957-7-39-347216-2', 'Learn Biology','Jack', 'XYZ', 400, 200,6)
 
-
-booklist=[book1, book2, book3, book4]
-booksfromjack=[]
-
-for i in booklist:
-    i.display()
-    if i.author=='Jack':
-        booksfromjack.append(i.title)
-
-for i in booksfromjack:
-    print(f'Book writen by Jack: {i}')
+book1.sell()
+book1.display()
