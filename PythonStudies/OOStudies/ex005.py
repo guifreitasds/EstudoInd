639097f588adbcb3250bb28c8707f0a3a3401336
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-17 10:35:15 -0300

    creating class Book

---
+++ PythonStudies/OOStudies/ex005.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self, isbn, title, author, publisher, pages, price, copies):
+        self.isbn=isbn
+        self.title=title
+        self.author=author
+        self.publisher=publisher
+        self.pages=pages
+        self.price=price
+        self.copies=copies
+
+
+
+book1 = Book('957-4-36-547417-1', 'Learn Physics','Stephen', 'CBC', 350, 200,0)
+book2 = Book('652-6-86-748413-3', 'Learn Chemistry','Jack', 'CBC', 400, 220,20)
+book3 = Book('957-7-39-347216-2', 'Learn Maths','John', 'XYZ', 500, 300,5)
+book4 = Book('957-7-39-347216-2', 'Learn Biology','Jack', 'XYZ', 400, 200,6)
+
