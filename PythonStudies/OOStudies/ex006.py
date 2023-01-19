0acbb34b6013d07f72ae12aba6ed186700867ccc
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-19 08:14:40 -0300

    reorganizating methods

--- PythonStudies/OOStudies/ex006.py
+++ PythonStudies/OOStudies/ex006.py
@@ -1,5 +1,5 @@
 class Fraction:
-    def __init__(self, nr, dr):
+    def __init__(self, nr, dr=1):
         if nr<0 and dr<0:
             self.nr = nr * -1
             self.dr = dr * -1
@@ -13,30 +13,20 @@ class Fraction:
     def display(self):
         return f'{self.nr}/{self.dr}'
 
-    def add(self, nr1, dr1, nr2, dr2=1):
-        numer = (nr1*dr2+nr2*dr1)
-        denom = dr1*dr2
+
+    def add(self,f2):
+        numer = (self.nr*f2.dr)+(self.dr*f2.nr)
+        denom = self.dr*f2.dr
         f4 = Fraction(numer, denom)
         print(f'Fraction solved: {f4.display()}')
         return f4
 
-    def addanotherform(self, f1,f2):
-        numer = (f1.nr*f2.dr)+(f1.dr*f2.nr)
-        denom = f1.dr*f2.dr
-        f4 = Fraction(numer, denom)
-        print(f'Fraction solved: {f4.display()}')
-        return f4
-    
-    def multiply(self, nr1, dr1, nr2, dr2=1):
-        numer = nr1*nr2
-        denom = dr1*dr2
-        f = Fraction(numer,denom)
-        print(f'Fraction solved: {f.display()}')
-        return f
 
-    def multiplyanotherform(self, f1, f2):
-        numer = f1.nr*f2.nr
-        denom = f1.dr*f2.dr
+    def multiply(self, f2):
+        if type(f2)==int:
+            f2 = Fraction(f2)
+        numer = self.nr*f2.nr
+        denom = self.dr*f2.dr
         f = Fraction(numer,denom)
         print(f'Fraction solved: {f.display()}')
         return f
@@ -49,6 +39,6 @@ a = int(input('Type a denominator: '))
 f1 = Fraction(x,y)
 f2 = Fraction(z, a)
 print(f1.display())
-f1.multiplyanotherform(f1,f2)
+f1.multiply(5)
 f1.add(f1.nr,f1.dr, f2.nr,f2.dr)
 f1.addanotherform(f1,f2)
