da964aaa638cb97986f3fff2e28da9caf2da3d81
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-19 07:48:51 -0300

    creating a multiply method

--- PythonStudies/OOStudies/ex006.py
+++ PythonStudies/OOStudies/ex006.py
@@ -12,9 +12,17 @@ class Fraction:
 
     def display(self):
         print(f'{self.nr}/{self.dr}')
-
+    
+    def multiply(self, nr1, dr1, nr2, dr2=1):
+        numer = nr1*nr2
+        denom = dr1*dr2
+        print(f'Fraction solved: {numer}/{denom}')
 x = int(input('Type a numerator: '))
 y = int(input('Type a denominator: '))
+z = int(input('Type a numerator: '))
+a = int(input('Type a denominator: '))
 
 f1 = Fraction(x,y)
+f2 = Fraction(z, a)
 f1.display()
+f1.multiply(f1.nr,f1.dr, f2.nr, f2.dr)
