3ecffffa82d0c027806f251f7212dc1a2b5b7ad4
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-20 09:58:25 -0300

    creating domain confimation in class employee

--- PythonStudies/OOStudies/ex010.py
+++ PythonStudies/OOStudies/ex010.py
@@ -1,11 +1,17 @@
 class Employee:
+    domains=set()
+    allowed_domains = {'yahoo.com', 'gmail.com', 'outlook.com'}
     def __init__(self, name, email):
         self.name = name
-        self.email = email
+        if email[email.index('@')+1:] in Employee.allowed_domains:
+            self.email = email
+        else:
+            raise RuntimeError()
+        Employee.domains.add(self.email[self.email.index('@'):])
 
 
-def display(self):
-    print(self.name, self.email)
+    def display(self):
+        print(self.name, self.email)
 
 
 e1 = Employee('John', 'john@gmail.com')
@@ -14,3 +20,7 @@ e3 = Employee('Jill', 'jill@outlook.com')
 e4 = Employee('Ted', 'ted@yahoo.com')
 e5 = Employee('Tim', 'tim@gmail.com')
 e6 = Employee('Mike', 'mike@yahoo.com')
+
+
+
+print(Employee.domains)
