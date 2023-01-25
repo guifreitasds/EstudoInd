de735ac65023559c7b1d21d182dc5ceaea1e9f9d
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-25 15:27:15 -0300

    calling init from superclass in subclass init

--- PythonStudies/OOStudies/ex001.py
+++ PythonStudies/OOStudies/ex001.py
@@ -17,12 +17,21 @@ class Person:
         print(f'Hello, i am {self.name}, how are you doing?')
 
 class Employee(Person):
-    pass
+    def __init__(self, name, age, salary, rg):
+        super().__init__(name, age)
+        self.salary=salary
+        self.rg=rg
+
+    def get_details(self):
+        print(f'Rg: {self.rg} // Employee: {self.name} // Salary: US${self.salary}')
+
 
 p1=Person("Jorge", 25)
-e1=Employee("Carlos", 19)
+e1=Employee("Carlos", 19, 1200, 1)
 
 p1.get_details()
 e1.greet()
 e1.get_details()
+
+
 
