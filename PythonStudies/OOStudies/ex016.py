d8e1985959be044ac73d4ae86bb760fd41918849
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-26 12:40:38 -0300

    creating toString method

--- PythonStudies/OOStudies/ex016.py
+++ PythonStudies/OOStudies/ex016.py
@@ -8,4 +8,10 @@ class Course:
         self.ratings=ratings
         self.avg_rating=avg_rating
 
-    
+    def __str__(self):
+        print(f"""{self.title}
+Instructor: {self.instructor}
+Rating: {self.avg_rating}
+""")
+
+    d
