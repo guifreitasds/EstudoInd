007a50f934fd43f41febf839badefe7e89c8d2dc
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-26 12:50:31 -0300

    finishing classes inheritances

--- PythonStudies/OOStudies/ex016.py
+++ PythonStudies/OOStudies/ex016.py
@@ -12,6 +12,29 @@ class Course:
         print(f"""{self.title}
 Instructor: {self.instructor}
 Rating: {self.avg_rating}
+Price: US${self.price}
 """)
 
-    d
+    def new_user(self, new):
+        self.users.append(new)
+
+    def receive_rating(self, val):
+        self.ratings.append(val)
+
+    def show_details(self):
+        print(f"""{self.lectures} Lectures
+{self.users.length()} Users 
+""")
+
+class VideoCourse(Course):
+    def __init__(self, title, instructor, price, lectures, users, ratings, avg_rating, leng):
+        super().__init__(title, instructor, price, lectures, users, ratings, avg_rating)
+        self.length_video=leng
+
+
+class PdfCourse(Course):
+    def __init__(self, title, instructor, price, lectures, users, ratings, avg_rating, pages):
+        super().__init__(title, instructor, price, lectures, users, ratings, avg_rating)
+        self.pages=pages
+
+
