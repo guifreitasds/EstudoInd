723cced09455d048898bdb058b256e89f5ef8513
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-26 12:38:44 -0300

    creating class course

---
+++ PythonStudies/OOStudies/ex016.py
@@ -0,0 +1,11 @@
+class Course:
+    def __init__(self, title, instructor, price, lectures, users, ratings, avg_rating):
+        self.title=title
+        self.instructor=instructor
+        self.price=price
+        self.lectures=lectures
+        self.users=users
+        self.ratings=ratings
+        self.avg_rating=avg_rating
+
+    
