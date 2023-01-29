320a4b47be7ef2c72db33cf8ac0533b50709dd11
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-29 16:26:48 -0300

    creating my first window

---
+++ PythonStudies/IntegrationsWSQL/login/index.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+
+#CRIANDO JANELA
+
+win = Tk()
+win.title('GRINGOTS Login - Acess Panel')
+win.geometry("600x300")
+win.configure(background="white")
+win.resizable(width=False, height=False)
+
+win.mainloop()
