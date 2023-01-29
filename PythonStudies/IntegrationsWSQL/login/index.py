5d1ea0d312869a789cae8ad75d95daa8320c1ca8
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-29 18:37:13 -0300

    adding widgets in the window

--- PythonStudies/IntegrationsWSQL/login/index.py
+++ PythonStudies/IntegrationsWSQL/login/index.py
@@ -1,12 +1,21 @@
+#Importar Libs
 from tkinter import *
 from tkinter import messagebox
 
 #CRIANDO JANELA
 
 win = Tk()
-win.title('GRINGOTS Login - Acess Panel')
+win.title('GRINGOTTS Login - Acess Panel')
 win.geometry("600x300")
 win.configure(background="white")
 win.resizable(width=False, height=False)
+
+
+# WIDGETS DA JANELA #
+leftframe=Frame(win, width=200, height=300, bg="MIDNIGHTBLUE", relief="raise")
+leftframe.pack(side=LEFT)
+
+rightframe=Frame(win, width=395, height=300, bg="MIDNIGHTBLUE", relief="raise")
+rightframe.pack(side=RIGHT)
 
 win.mainloop()
