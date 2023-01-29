59f1d0b30003a52c2d0b3ff54bbb3e8260e0dff1
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-29 18:40:36 -0300

    adding logo to the window

--- PythonStudies/IntegrationsWSQL/login/index.py
+++ PythonStudies/IntegrationsWSQL/login/index.py
@@ -5,11 +5,14 @@ from tkinter import messagebox
 #CRIANDO JANELA
 
 win = Tk()
-win.title('GRINGOTTS Login - Acess Panel')
+win.title('DP SYSTEMS - Login - Acess Panel')
 win.geometry("600x300")
 win.configure(background="white")
 win.resizable(width=False, height=False)
 
+
+# CARREGANDO IMAGENS #
+logo=PhotoImage(file="icons/logo.png")
 
 # WIDGETS DA JANELA #
 leftframe=Frame(win, width=200, height=300, bg="MIDNIGHTBLUE", relief="raise")
@@ -17,5 +20,8 @@ leftframe.pack(side=LEFT)
 
 rightframe=Frame(win, width=395, height=300, bg="MIDNIGHTBLUE", relief="raise")
 rightframe.pack(side=RIGHT)
+
+logolabel=Label(leftframe, image=logo, bg="MIDNIGHTBLUE")
+logolabel.place(x=50, y=100)
 
 win.mainloop()
