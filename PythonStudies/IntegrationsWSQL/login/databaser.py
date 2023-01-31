6cad2eadb50b7f21d0b290d1187f9e6ecc818d95
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-01-31 09:20:04 -0300

    creating table users with sqlite3

--- PythonStudies/IntegrationsWSQL/login/databaser.py
+++ PythonStudies/IntegrationsWSQL/login/databaser.py
@@ -1,6 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db")
+conn = sqlite3.connect("users_data.db")
 
 cursor = conn.cursor()
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id integer AUTOINCREMENT NOT NULL,
+    name varchar(40) NOT NULL,
+    mail text NOT NULL,
+    username varchar(20) NOT NULL,
+    password varchar(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+""")
