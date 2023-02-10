b5564fd55d1e6d036a3a233e958b86d52e3b1d4e
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-02-09 23:01:37 -0300

    reorder rep

--- PythonStudies/MontyPythonPET/ExercisesAula02/ex01.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Digite o 1° valor: '))
-n2 = int(input('Digite o 2° valor: '))
-
-print(f'=======================\nAdição = {n1+n2} \nSubtração = {n1-n2} \nMultiplicação = {n1*n2} \nDivisão = {n1/n2}\n=======================')
--- PythonStudies/MontyPythonPET/ExercisesAula02/ex03.py
+++
@@ -1,9 +0,0 @@
-time = float(input('Digite o tempo da sua viagem em horas: '))
-averagev = float(input('Digite a velocidade média da sua viagem em Km/h: '))
-
-distance = time*averagev
-
-used_l = distance/12
-
-print('\n==========VIAGEM COMPLETA==========')
-print(f'Você obteve uma velocidade média de {averagev}Km/h\nGastou {time} horas\nPercorreu uma distância de {distance}Km\nE acabou gastando {used_l}L de Combustível\n===================================')
