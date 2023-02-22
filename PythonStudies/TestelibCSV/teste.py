c0a6ab27f6eeb419843f66e1edfc30fd5e40a7c3
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-02-22 11:00:14 -0300

    finalizando treino de matrizes e código leitor/escritor

--- PythonStudies/TestelibCSV/teste.py
+++ PythonStudies/TestelibCSV/teste.py
@@ -1,17 +1,29 @@
 import csv
 
+# mtall = []
+# lctrl = []
+# fieldnames = ['ID', 'TIPO_PIPOCA', 'QTD_PIPOCA', 'VALOR_PIPOCA', 'VALOR_TOTAL', 'DATA', 'HORÁRIO']
+# with open('teste.csv', newline='', encoding='utf-8') as t:
+#     reader = csv.reader(t, delimiter=',')
+#     for i, row in enumerate(reader):
+#         for a in range(0, len(fieldnames)):
+#             lctrl.append(row[a])
+#         mtall.append(lctrl)
+#         lctrl = []
+#         with open('testewrite.csv', 'w', newline='') as g:
+#             writer = csv.DictWriter(g, fieldnames=fieldnames)
+#             for a in range(0, len(mtall)):
+#                 writer.writerow({f'ID': mtall[a][0], 'TIPO_PIPOCA': mtall[a][1], 'QTD_PIPOCA': mtall[a][2], 'VALOR_PIPOCA': mtall[a][3], 'VALOR_TOTAL': mtall[a][4], 'DATA': mtall[a][5],
+#                 'HORÁRIO': mtall[a][6]})
 
+with open('teste.csv', 'r') as t:
+    reader = csv.reader(t)
+    with open('testewrite.csv', 'w') as g:
+        writer = csv.writer(g)
+        for row in reader:
+            if '{2}' in row:
+                pass
+            else:
+                writer.writerow(row)
 
-with open('teste.csv', newline='', encoding='utf-8') as t:
-      reader = csv.reader(t, delimiter=',')
-      for id, row in enumerate(reader):
-        print(id, row)
-#with open('teste.csv','w', newline='') as t:
-#      fieldnames = ['ID', 'TIPO_PIPOCA', 'QTD_PIPOCA', 'VALOR_PIPOCA', 'VALOR_TOTAL', 'DATA', 'HORÁRIO']
-#      writer = csv.DictWriter(t, fieldnames=fieldnames)
-#      for i in range(0,5):
-#          id+=1
-#          tipo=1
-#          writer.writerow({f'ID': id, 'TIPO_PIPOCA': 'Doce' if tipo==1 else 'Salgada', 'QTD_PIPOCA': 1, 'VALOR_PIPOCA': {3 if tipo==1 else 2}, 'VALOR_TOTAL': 3.00, 'DATA': 15,
-#               'HORÁRIO': 16})
 
