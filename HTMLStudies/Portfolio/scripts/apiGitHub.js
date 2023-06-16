fbeda59168cf22812ab61e1a6f023fc277dae61f
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-06-16 15:44:44 -0300

    consuming github repos API

---
+++ HTMLStudies/Portfolio/scripts/apiGitHub.js
@@ -0,0 +1,35 @@
+// Write Javascript code!
+const ul = document.querySelector('ul');
+
+
+function getApiGitHub() {
+  fetch('https://api.github.com/users/guifreitasds/repos')
+    .then(async (res) => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      var data = await res.json();
+
+      data.map((item) => {
+        let li = document.createElement('li');
+
+        if(item.description==null){
+            item.description="Sem descrição"
+        }
+        li.innerHTML = `
+        <strong>${item.name} Repo</strong>
+        <span>${item.description}</span>
+        <span><a href='${item.html_url}'>Clique aqui para acessá-lo</a> </span>
+        <span>Data criação: 
+          ${Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}
+        </span>
+        `;
+
+        ul.appendChild(li);
+      });
+    })
+    .catch((e) => console.log(e));
+}
+
+getApiGitHub();
