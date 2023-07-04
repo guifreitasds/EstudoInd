f716c2a05d8d47fe00850dff0619d323f0a1e6a3
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-07-04 15:31:34 -0300

    add link to all commits in each repo

--- HTMLStudies/Portfolio/scripts/apiGitHubCommits.js
+++ HTMLStudies/Portfolio/scripts/apiGitHubCommits.js
@@ -1,4 +1,7 @@
+
 function getCommits(repo) {
+    let divcommits = document.querySelector('div.commits');
+    divcommits.innerHTML=``
     fetch(`https://api.github.com/repos/guifreitasds/${repo}/commits`)
       .then(async (res) => {
         if(!res.ok){
@@ -7,8 +10,7 @@ function getCommits(repo) {
 
         var data = await res.json();
 
-        let divcommits = document.querySelector('div.commits');
-        divcommits.innerHTML=``;
+
         
         for (let i = 0; i < 7; i++) {
             const item = data[i];
@@ -19,14 +21,24 @@ function getCommits(repo) {
 
             
             divintocommits.innerHTML = `
-            <a class="commit-one shadow-box" href="https://github.com/guifreitasds/${repo}/commit/${sha}">
+            <a class="commit-one shadow-box" target="_blank" href="https://github.com/guifreitasds/${repo}/commit/${sha}">
                 <strong>${item.commit.message}</strong>
                 <span>${item.commit.author.date}</span>
             </a>
             `;
+
+
             
             divcommits.appendChild(divintocommits);
+
         }
       })
       .catch((e) => console.log(e));
+      setTimeout(()=>{
+        let seecommits = document.createElement('div')
+        seecommits.innerHTML = `<a class="seecommits" href="https://github.com/guifreitasds/${repo}/commits/main" target="_blank">See all commits</a>`
+        divcommits.appendChild(seecommits)
+
+      }, 500)
+
 }
