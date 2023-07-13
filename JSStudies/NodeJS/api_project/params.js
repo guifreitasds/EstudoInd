0fe752719343f15c368e6725328a458307a24659
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-07-13 07:35:41 -0300

    making a get with route params

--- JSStudies/NodeJS/api_project/params.js
+++ JSStudies/NodeJS/api_project/params.js
@@ -5,7 +5,11 @@ app.listen('3000');
 
 app.use(exp.json())
 
-app.route('/').post( (req, res) => {
-    const {nome, cidade, times_favoritos} = req.body
-    res.send(`Meu nome é ${nome}, moro em ${cidade} e meus times favoritos são ${times_favoritos}`)
-} )
+// app.route('/').post( (req, res) => {
+//     const {nome, cidade, times_favoritos} = req.body
+//     res.send(`Meu nome é ${nome}, moro em ${cidade} e meus times favoritos são ${times_favoritos}`)
+// } )
+
+app.route('/').get( (req, res) => res.send(`Olá`) )
+
+app.route('/:nome').get( (req, res) => res.send(`Meu nome é ${req.params.nome}`) )
