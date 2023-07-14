6e0a2ec2ca1f97e5bc0f525871be95cb5c075620
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-07-14 12:17:43 -0300

    making a POST in a local API

--- JSStudies/NodeJS/fetch_discover_api/main.js
+++ JSStudies/NodeJS/fetch_discover_api/main.js
@@ -6,18 +6,39 @@ function getUsers() {
     .then( data => document.querySelector("#renderapi").innerHTML=`${JSON.stringify(data)}` )
     .catch( e => console.error(e) )
 }
-
-getUser(1)
+getUsers()
+getUser(2)
+addUser()
 
 function getUser(id) {
     fetch(`${url}/${id}`)
     .then( (response) => response.json() )
     .then( data => {
-        document.querySelector('#user').innerHTML=`Seu user é: ${data.name}`
-        document.querySelector("#userCity").innerHTML=`Sua cidade é: ${data.city}`
+        user.innerHTML=`Seu user é: ${data.name}`
+        userCity.innerHTML=`Sua cidade é: ${data.city}`
         userAvatar.src=`${data.avatar}`
     } )
     .catch( e => console.error(e) )
 }
 
 
+function addUser() {
+    const newUser = {
+        name: "guifreitasds",
+        avatar: "https://avatars.githubusercontent.com/u/99972010?v=4",
+        city: "Recife"
+    }
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then( (response) => response.json() )
+        .then( data => {
+            alertApi.textContent=`${data}`
+        })
+        .catch( e => console.error(e))
+
+}
