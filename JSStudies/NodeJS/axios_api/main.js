91444482eab39d15dec88b6f67dfedd2fc789e3e
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-07-17 08:48:16 -0300

    making a post with axios

--- JSStudies/NodeJS/axios_api/main.js
+++ JSStudies/NodeJS/axios_api/main.js
@@ -1,11 +1,26 @@
-const url = `http://localhost:5500/api`;
+const url = `http://localhost:5500/api/2`;
 
-function getUser() {
+function getUser(url) {
     axios.get(url)
         .then(response => {
             users.innerHTML = `${JSON.stringify(response.data)}`
+            userAvatar.src = `${response.data.avatar}`
         })
         .catch(e => console.log(e))
 }
 
-getUser()
+const user = {
+    name: "Guilherme Freitas",
+    avatar: "https://avatars.githubusercontent.com/u/99972010?v=4",
+    city: "Recife"
+}
+
+function addUser(user) {
+    axios.post(url, user)
+        .then(response => console.log(response))
+        .catch(e => console.log(e))
+}
+
+getUser(url)
+
+// addUser(user)
