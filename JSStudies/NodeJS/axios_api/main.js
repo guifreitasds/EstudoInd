41613e71f0340acf791b6ddb61abb336fa6d6757
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-07-17 08:55:59 -0300

    making a put with axios

--- JSStudies/NodeJS/axios_api/main.js
+++ JSStudies/NodeJS/axios_api/main.js
@@ -1,7 +1,7 @@
 const url = `http://localhost:5500/api/`;
 
 function getUser(id) {
-    axios.get(`http://localhost:5500/api/${id}`)
+    axios.get(`${url}${id}`)
         .then(response => {
             users.innerHTML = `${JSON.stringify(response.data)}`
             userAvatar.src = `${response.data.avatar}`
@@ -15,12 +15,26 @@ const user = {
     city: "Recife"
 }
 
+const updatedUser = {
+    name: "Lucas",
+    avatar: "https://avatars.githubusercontent.com/u/99782517?v=4",
+    city: "Recife"
+}
+
 function addUser(user) {
     axios.post(url, user)
         .then(response => console.log(response))
         .catch(e => console.log(e))
 }
 
+function updateUser(id, user) {
+    axios.put(`${url}${id}`, user)
+        .then(response => console.log(response))
+        .catch(e => console.log(e))
+}
+
 getUser(2)
+
+updateUser(2, updatedUser)
 
 // addUser(user)
