36edc04f127b821c8816f3941d1863430cc6ed81
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-08-03 20:33:36 -0300

    first django project

---
+++ PythonStudies/api_todolist/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from app.views import TodoViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+
+
+urlpatterns = router.urls
+
